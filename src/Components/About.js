fa438a2bb14feb2c674393fafdd6ea7372423e2f
Joyce Zhang <jzhang4@colgate.edu>
2022-04-09 18:46:17 -0400

    about v1

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,52 +4,60 @@ class About extends Component {
   render() {
 
     if(this.props.data){
-      // var name = this.props.data.name;
-      // var profilepic= "images/"+this.props.data.image;
-      // var bio = this.props.data.bio;
-      // var street = this.props.data.address.street;
-      // var city = this.props.data.address.city;
-      // var state = this.props.data.address.state;
-      // var zip = this.props.data.address.zip;
-      // var phone= this.props.data.phone;
-      // var email = this.props.data.email;
-      // var resumeDownload = this.props.data.resumedownload;
-      var name = "Akuna";
-      var akuna = `this.props.data[${name}]`;
-      console.log(akuna);
+       var akuna = this.props.data['Akuna'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       })
+
+       var audible = this.props.data['Audible'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       }) 
+       var bloomberg = this.props.data['Bloomberg'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       })
     }
 
     return (
       <section id="about">
-      {/* <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>Akuna</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {akuna}
             </div>
          </div>
-      </div> */}
-
+   
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>Audible</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {audible}
+            </div>
+         </div> 
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>Bloomberg</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {bloomberg}
+            </div>
+         </div>
    </section>
     );
   }
