944f921a2f676cd75d7f4a53bd4bb096ca060863
Joyce Zhang <jzhang4@colgate.edu>
2022-04-09 19:54:31 -0400

    company v2

--- src/Components/About.js
+++ src/Components/About.js
@@ -29,10 +29,98 @@ class About extends Component {
            <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
          </div>
        })
-    }
+       var C3 = this.props.data['C3AI'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       })
+       var databricks = this.props.data['Databricks'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       })
+       
+       var microsoft = this.props.data['Microsoft'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       })
+       
+       var gs = this.props.data['Godman Sachs'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       })
+       var salesforce = this.props.data['Salesforce'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       }) 
+      
+       var tiktok = this.props.data['Tiktok'].questions.map(function(n){
+         var name = n.title.toLowerCase();
+         name =name.replace(/\s+/g, '-');
+         var url ="https://leetcode.com/problems/"+name+"/";
+         return <div key={n.id}>
+           <a title="Styleshout" href={url} target="_blank" rel="noreferrer noopener">{n.id}</a>. {n.title}
+         </div>
+       }) 
+      }
 
     return (
       <section id="about">
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>Godman Sachs</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {gs}
+            </div>
+         </div>
+         
+         
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>Microsoft</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {microsoft}
+            </div>
+         </div>
+
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>Salesforce</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {salesforce}
+            </div>
+         </div> 
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>Tiktok</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {tiktok}
+            </div>
+         </div>
+
+
          <div className="row type">
             <div className="three columns header-col">
                <h2><span>Akuna</span></h2>
@@ -58,7 +146,25 @@ class About extends Component {
                {bloomberg}
             </div>
          </div>
-   </section>
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>C3 AI</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {C3}
+            </div>
+         </div>
+         <div className="row type">
+            <div className="three columns header-col">
+               <h2><span>Databricks</span></h2>
+            </div>
+            <div className="nine columns main-col">
+               {databricks}
+            </div>
+         </div>
+         
+
+         </section>
     );
   }
 }
